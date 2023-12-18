53b5f9078c7321d4bf4387d7b8bacbc38d5d3015
Arjun-Gc <136101740+Arjun-Gc@users.noreply.github.com>
2023-12-18 12:24:02 +0545

    Add files via upload

---
+++ ETL/simple etl.py
@@ -0,0 +1,34 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+def extract():
+    """ This API extracts data from
+    http://universities.hipolabs.com
+    """
+    API_URL = "http://universities.hipolabs.com/search?country=United+States"
+    data = requests.get(API_URL).json()
+    return data
+
+def transform(data) :
+    """ Transforms the dataset into desired structure and filters"""
+    df = pd.DataFrame(data)
+    print(f"Total Number of universities from API {len(data)}")
+    df = df[df["name"].str.contains("California")]
+    print(f"Number of universities in california {len(df)}")
+    df['domains'] = [','.join(map(str, l)) for l in df['domains']]
+    df['web_pages'] = [','.join(map(str, l)) for l in df['web_pages']]
+    df = df.reset_index(drop=True)
+    return df[["domains","country","web_pages","name"]]
+
+def load(df):
+    """ Loads data into a sqllite database"""
+    disk_engine = create_engine('sqlite:///sqlite_store.db')
+    df.to_sql('california_uni', disk_engine, if_exists='replace')
+
+
+data = extract()
+df = transform(data)
+load(df)
+
+
